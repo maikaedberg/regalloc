f4c883a240c6a3d5fa9c2b62e3aa0c1f8f8ede81
maikaedberg <69947279+maikaedberg@users.noreply.github.com>
2021-11-25 15:12:05 +0100

    added max-cardinality-search algo

--- intergraph.py
+++ intergraph.py
@@ -2,8 +2,8 @@ from cfg import CFG
 
 class InterGraph():
     def __init__(self, cfg):
-        self.nodes = []
-        self.edges = {}
+        self.nodes = ['c','b','a','d']
+        self.edges = {'a' : [], 'b' : [], 'c' : ['d'], 'd' : ['c']}
         self.spilled = []
         self.build_graph(cfg)
     
@@ -12,7 +12,22 @@ class InterGraph():
 
     def max_cardinality_search(self):
         """Returns a SEO from the current interference graph"""
-        pass
+
+        vertex = self.nodes[0]
+        SEO = [vertex]
+        cards = {v : 0 for v in self.edges if v != vertex}
+
+        while len(SEO) < len(self.nodes):
+            for v in self.edges[vertex]:
+                if v in cards:
+                    cards[v] += 1
+            max_card = max(cards.values())
+            vp = list(cards.keys())[list(cards.values()).index(max_card)]
+            SEO.append(vp)
+            cards.pop(vp)
+
+        return SEO
+
 
     def spill(self, temporary):
         pass
